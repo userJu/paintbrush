06ae88920d57ae3ad03eec231e0d3e0723951665
JuhyunLim <87933367+userJu@users.noreply.github.com>
2021-10-09 19:54:42 +0900

    Js : color btn active

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 //detact 'mouse in canvas'
 const canvas = document.querySelector('#jsCanvas')
+const ctx = canvas.getContext('2d');
+const colors = document.querySelectorAll('.controls__color')
 
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle= "#2c2c2c"
+ctx.lineWidth = 2.5;
 let painting = false;
 
 function stopPainting(){
     painting=false
 }
+function startPainting(){
+    painting = true
+}
 canvas.addEventListener('mousemove',(e)=>{
     const x = e.offsetX;
     const y = e.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else{
+        ctx.lineTo(x,y)
+        ctx.stroke()
+    }
 })
 canvas.addEventListener('mousedown',(e)=>{
-    painting = true;
+    startPainting()
     paint()
 
 })
@@ -30,3 +47,12 @@ function paint(){
         console.log('painting is end')
     }
 }
+
+
+console.log(colors)
+colors.forEach(color => 
+    color.addEventListener('click',(e)=>{
+        const target = e.target.style.backgroundColor;
+        ctx.strokeStyle = target;
+    }))
+
